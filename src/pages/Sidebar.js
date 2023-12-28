a4f146634bf530566cfed1028a347cf7a19cdf35
OlaniyiHope <olaniyihoppee@gmail.com>
2023-12-28 15:50:07 +0100

    all

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -35,18 +35,6 @@ const Sidebar = () => {
 
               <li class="menu--item">
                 <a
-                  href="my_organisation_dashboard_contact_lists.html"
-                  class="menu--link"
-                  title="Contact List"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="right"
-                >
-                  <i class="fa-regular fa-address-card menu--icon"></i>
-                  <span class="menu--label">Contact List</span>
-                </a>
-              </li>
-              <li class="menu--item">
-                <a
                   href="/dashboard-payout"
                   class="menu--link"
                   title="Payouts"
@@ -69,30 +57,7 @@ const Sidebar = () => {
                   <span class="menu--label">Reports</span>
                 </a>
               </li>
-              <li class="menu--item">
-                <a
-                  href="my_organisation_dashboard_subscription.html"
-                  class="menu--link"
-                  title="Subscription"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="right"
-                >
-                  <i class="fa-solid fa-bahai menu--icon"></i>
-                  <span class="menu--label">Subscription</span>
-                </a>
-              </li>
-              <li class="menu--item">
-                <a
-                  href="my_organisation_dashboard_conversion_setup.html"
-                  class="menu--link"
-                  title="Conversion Setup"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="right"
-                >
-                  <i class="fa-solid fa-square-plus menu--icon"></i>
-                  <span class="menu--label">Conversion Setup</span>
-                </a>
-              </li>
+
               <li class="menu--item">
                 <a
                   href="/dashboard-profile"
